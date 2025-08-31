cd2a170b9e45ee1500435e2a29224f812ad9cab5
Pedro Juan <pedrosaraivaofc@gmail.com>
2025-08-31 18:40:01 -0300

    chore(ios): projeto iOS inicial (SwiftUI)

---
+++ Habitoo/HabitooApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitooApp.swift
+//  Habitoo
+//
+//  Created by Pedro Juan Ferreira Saraiva on 31/08/25.
+//
+
+import SwiftUI
+
+@main
+struct HabitooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
